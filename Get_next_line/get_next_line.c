0bdaf2d158dad32363548ea927bc8eec2b328132
Alexandre Martinod <amartinod@MacBook-Pro-de-Alexandre.local>
2019-01-28 15:15:19 +0100

    avant gros changement

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -27,30 +27,37 @@ int		get_next_line(const int fd, char **line)
 		return (-1);
 
 	char		buff[BUFF_SIZE + 1];
-	//static char	*s;
-	char		*str_total;
+	char		*s;
+	char static	*str_total;
 	int			i;
 	int			tmp;
 	int			j;
 
-	i = 0;
-	j = 0;
+	i = 0;  //i et j se réinitialise a chaque appel de gnl. 
+	j = 0;	//je dois garder un trace de ou j'en etais
 	while ((tmp = read(fd, &buff, BUFF_SIZE)))
 	{
 		buff[BUFF_SIZE] = '\0';
-		if (!str_total)
+		if (!s)
+		{
 			str_total = ft_strdup(buff);
+			s = ft_strdup(buff);
+		}
 		else
+		{
 			str_total = ft_strjoin(str_total, buff);
+		}
 		ft_strclr(buff);
 		while (str_total[j])
 		{
 			if (str_total[j] == '\n')
 			{
+				printf("\033[34;01m%s\033[00m\n", str_total);
 				if (!(*line = (char*)malloc(sizeof(char) * (j - i + 1))))
 					return (0);
 				*line = ft_memmove(*line, &(str_total[i]), (j - i));
 				(*line)[j - i] = '\0';
+				
 				i = j;
 				return (1);
 			}
@@ -70,3 +77,15 @@ int		get_next_line(const int fd, char **line)
 		return (tmp);
 	return (1);
 }
+
+/* verifier si j'ai deja read quelque chose 
+parcourir a la recherche de \n
+		quand je tombe sur un \n je memmove sur ma *line puis j'avance sur str_total
+
+si rien de nouveau read again
+parcourir a la recherche de \n
+		quand je tombe sur un \n je memmove sur ma *line puis j'avance sur str_total
+
+quand j'ai fini de read, parcourir la chaine a la recherche de \n 
+		quand je tombe sur un \n je memmove sur ma *line puis j'avance sur str_total
+*/
