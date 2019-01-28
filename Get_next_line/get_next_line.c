d41275002311a2d7b6f29e609dae6512a8a88ed7
Alexandre MARTINOD <amartino@e1r2p19.42.fr>
2019-01-28 19:17:51 +0100

    PROBLEME

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amartino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 14:19:34 by amartino          #+#    #+#             */
-/*   Updated: 2019/01/27 20:22:03 by amartino         ###   ########.fr       */
+/*   Updated: 2019/01/28 19:17:09 by amartino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,71 +21,91 @@ int		ft_checks(const int fd)
 	return (0);
 }
 
+char	*ft_get_line(char *str_total, char **line, int i)
+{
+	if (!(*line = (char*)malloc(sizeof(char) * (i + 1))))
+		return (0);
+	*line = ft_memmove(*line, str_total, i);
+	(*line)[i] = '\0';
+	str_total = str_total + i;
+		printf("\033[32;01m%s\033[00m\n", str_total);  //vert
+	return (str_total);
+}
+
+int		ft_parse_line(char *str_total)
+{
+	int		i;
+
+	i = -1;
+	while (str_total[++i])
+	{
+		if (str_total[i] == '\n')
+		{
+		//	str_total = ft_get_line(str_total, line, i);
+			return (i);
+		}
+	}
+	return (0);
+}
+
 int		get_next_line(const int fd, char **line)
 {
 	if (ft_checks(fd) == -1)
 		return (-1);
 
-	char		buff[BUFF_SIZE + 1];
-	char		*s;
-	char static	*str_total;
-	int			i;
-	int			tmp;
-	int			j;
+	char			buff[BUFF_SIZE + 1];
+	char static		*str_total;
+	int				ret;
+	int				i;
 
-	i = 0;  //i et j se réinitialise a chaque appel de gnl. 
-	j = 0;	//je dois garder un trace de ou j'en etais
-	while ((tmp = read(fd, &buff, BUFF_SIZE)))
+	i = 0;
+	printf("\033[31;01m%s\033[00m\n", str_total);   //rouge
+//	if (str_total && *str_total)
+//	{
+//		if (ft_parse_line(str_total, line))
+//			return (1);
+//	}
+	while ((ret = read(fd, &buff, BUFF_SIZE)))
 	{
 		buff[BUFF_SIZE] = '\0';
-		if (!s)
-		{
+		if (!str_total)
 			str_total = ft_strdup(buff);
-			s = ft_strdup(buff);
-		}
 		else
-		{
 			str_total = ft_strjoin(str_total, buff);
-		}
 		ft_strclr(buff);
-		while (str_total[j])
+		printf("\033[33;01mSalut\033[00m\n");  //jaune
+		while (str_total[i])  //semblerai que le i s'incremente mal en tout cas on ne rentre pas dans if
 		{
-			if (str_total[j] == '\n')
+			printf("\033[34;01mSalut\033[00m\n");  //bleu
+			if ((i = ft_parse_line(str_total)) != 0)
 			{
-				printf("\033[34;01m%s\033[00m\n", str_total);
-				if (!(*line = (char*)malloc(sizeof(char) * (j - i + 1))))
-					return (0);
-				*line = ft_memmove(*line, &(str_total[i]), (j - i));
-				(*line)[j - i] = '\0';
-				
-				i = j;
+				str_total = ft_get_line(str_total, line, i);
 				return (1);
 			}
-			j++;
+			i++;
 		}
 	}
-	if (i < j)
-	{
-		if (!(*line = (char*)malloc(sizeof(char) * (j - i + 1))))
-			return (0);
-		*line = ft_memmove(*line, &(str_total[i]), (j - i));
-		(*line)[j - i] = '\0';
-		i = j;
-		return (1);
-	}
-	if (tmp == 0 || tmp == -1)
-		return (tmp);
-	return (1);
+	//if (*str_total && str_total)
+	//{
+	//	if (ft_parse_line(str_total, *line))
+	//		return (1);
+	//}
+	if (ret == 0 || ret == -1)
+		return (ret);
+	return (0);
 }
 
-/* verifier si j'ai deja read quelque chose 
-parcourir a la recherche de \n
-		quand je tombe sur un \n je memmove sur ma *line puis j'avance sur str_total
-
-si rien de nouveau read again
-parcourir a la recherche de \n
-		quand je tombe sur un \n je memmove sur ma *line puis j'avance sur str_total
-
-quand j'ai fini de read, parcourir la chaine a la recherche de \n 
-		quand je tombe sur un \n je memmove sur ma *line puis j'avance sur str_total
+/*bon le probleme ma variable static. 
+ *
+ *
+ *
+ *
+ * verifier si j'ai deja read quelque chose
+ * if parse then get_line
+ *
+ * si rien de nouveau read again, je strjoin mon buffer vers str_total
+ * if parse then get_line
+ *
+ * quand j'ai fini de read
+ * if parse then get_line
 */
