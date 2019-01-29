db0c7d870f8fc660d250b13005f8eb6cf6364302
Alexandre MARTINOD <amartino@e1r2p22.42.fr>
2019-01-29 19:03:08 +0100

    petit changement

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amartino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 14:19:34 by amartino          #+#    #+#             */
-/*   Updated: 2019/01/28 19:17:09 by amartino         ###   ########.fr       */
+/*   Updated: 2019/01/29 16:05:32 by amartino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,27 @@ int		ft_checks(const int fd)
 	return (0);
 }
 
-char	*ft_get_line(char *str_total, char **line, int i)
+char	*ft_get_line(char *str_total, char **line, int j)
 {
-	if (!(*line = (char*)malloc(sizeof(char) * (i + 1))))
+	if (!(*line = (char*)malloc(sizeof(char) * (j + 1))))
 		return (0);
-	*line = ft_memmove(*line, str_total, i);
-	(*line)[i] = '\0';
-	str_total = str_total + i;
-		printf("\033[32;01m%s\033[00m\n", str_total);  //vert
+	*line = ft_memmove(*line, str_total, j);
+	(*line)[j] = '\0';
+	printf("\033[32;01m%s\033[00m\n", str_total);  //vert
+	str_total = str_total + j;
+	printf("\033[32;01m%sincrémenté\033[00m\n", str_total);  //vert
 	return (str_total);
 }
 
 int		ft_parse_line(char *str_total)
 {
-	int		i;
+	int		j;
 
-	i = -1;
-	while (str_total[++i])
+	j = -1;
+	while (str_total[++j])
 	{
-		if (str_total[i] == '\n')
-		{
-		//	str_total = ft_get_line(str_total, line, i);
-			return (i);
-		}
+		if (str_total[j] == '\n')
+			return (j);
 	}
 	return (0);
 }
@@ -57,9 +55,9 @@ int		get_next_line(const int fd, char **line)
 	char static		*str_total;
 	int				ret;
 	int				i;
+	int				j;
 
 	i = 0;
-	printf("\033[31;01m%s\033[00m\n", str_total);   //rouge
 //	if (str_total && *str_total)
 //	{
 //		if (ft_parse_line(str_total, line))
@@ -72,24 +70,35 @@ int		get_next_line(const int fd, char **line)
 			str_total = ft_strdup(buff);
 		else
 			str_total = ft_strjoin(str_total, buff);
+		printf("\033[34;01m%s\033[00m\n", buff);  //bleu
 		ft_strclr(buff);
-		printf("\033[33;01mSalut\033[00m\n");  //jaune
 		while (str_total[i])  //semblerai que le i s'incremente mal en tout cas on ne rentre pas dans if
 		{
-			printf("\033[34;01mSalut\033[00m\n");  //bleu
-			if ((i = ft_parse_line(str_total)) != 0)
+			printf("\033[31;01m%s\033[00m\n", str_total);   //rouge
+			if ((j = ft_parse_line(str_total)) != 0)
 			{
-				str_total = ft_get_line(str_total, line, i);
+				str_total = ft_get_line(str_total, line, j);
 				return (1);
 			}
 			i++;
 		}
 	}
-	//if (*str_total && str_total)
-	//{
-	//	if (ft_parse_line(str_total, *line))
-	//		return (1);
-	//}
+	printf("\033[33;01m%d\033[00m\n", ret);  //jaune
+	buff[BUFF_SIZE] = '\0';
+	printf("\033[31;01m%s\033[00m\n", str_total);   //rouge
+	str_total = ft_strjoin(str_total, buff);
+	printf("\033[31;01m%s\033[00m\n", str_total);   //rouge
+	ft_strclr(buff);
+	if (*str_total && str_total)
+	{
+		printf("\033[33;01mEt la ?\033[00m\n");  //jaune
+		if ((j = ft_parse_line(str_total)) != 0)
+		{
+			printf("\033[33;01mEt la ?\033[00m\n");  //jaune
+			str_total = ft_get_line(str_total, line, i);
+			return (1);
+		}
+	}
 	if (ret == 0 || ret == -1)
 		return (ret);
 	return (0);
