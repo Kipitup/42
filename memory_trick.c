8d77b84d3c15c68be8040fe358c7e080ccebde41
amartino <a.martinod1@gmail.com>
2020-03-29 19:34:18 +0200

    add some new tricky stuff to memory trick file

--- memory_trick.c
+++ memory_trick.c
@@ -4,16 +4,24 @@ int	main(int ac, char **av)
 {
 	//(void)ac;
 	//(void)av;
+	printf("((int[]){2,1})[1] %d\n", ((int[]){2,1})[1] );
 	long	tab[2] = {42, 38};
 	char	*str;
 
-	printf("%s\n%s\n", *((char **)tab[-2]), av[0]);
+	printf("tab[1]     %ld\n", tab[1]);
+	printf("*(tab + 1) %ld\n", *(tab + 1));
+	printf("1[tab]     %ld\n", 1[tab]);
+	printf("*tab     %ld\n", *tab);
+	
+	printf("*((char **)tab[-2]) %s\n", *((char **)tab[-2]));
+	printf("av[0]\t\t    %s\n", av[0]);
 
 	printf("addr of av:   \t\t%p\n", av);
 	printf("addr of tab[-2]:\t%p\n", tab[-2]);
 	printf("addr of tab:     \t%p\n", tab);
 	printf("addr of av[0]:\t\t%p\n", av[0]);
 
+	
 	// str = *((char **)tab[-2]);
 	// printf("%s\n%s\n", str, av[0]);
 	// str[0] = 'H';
